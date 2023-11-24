3fd5a5cc67cd8745dfafc5361a39069bf85c5cbc
MALEX1233 <114114185+MaxyMunoz@users.noreply.github.com>
2023-11-24 15:39:50 -0300

    24-11-23

--- Godot/Script/player.gd
+++ Godot/Script/player.gd
@@ -1,52 +1,54 @@
 extends CharacterBody2D
 
-var velocidad =40
-const  gravedad = 300
-var salto=200
-var screen_size
-var ani
-
-func _ready():
-	screen_size=get_viewport_rect().size
+var velocidad : int = 30
+var salto : int = 200
+var gravedad : int = 300
+var accion : String
+var atacar : bool = false
 
 func _physics_process(delta):
+	
 	velocity.y += gravedad*delta
-	if Input.is_action_pressed("CAMINAR"):
-		velocity.x= velocidad
-		ani=1
-	
-	elif  Input.is_action_pressed("RETROCEDER"):
-		velocity.x = -velocidad
-		ani=-1
-	elif  Input.is_action_pressed("correr atras"):
-		velocity.x =- velocidad*3
-		ani =-2
-	else:
-		velocity.x = 0
-		ani=0
-		$AnimatedSprite2D.play("detenido")
-	if Input.is_action_pressed("CORRER adelante"):
-		velocity.x=velocidad*3
-		ani=2
-	elif  Input.is_action_pressed("correr atras"):
-		velocity.x =- velocidad*3
-		ani =-2
-	if velocity.x == 0:
-		velocity.x = 0
-		ani=0
-		$AnimatedSprite2D.play("detenido")
-	if is_on_floor():
-		if Input.is_action_just_released("SALTAR"):
-			velocity.y =-salto
+	if atacar == false:
+		if Input.is_action_pressed("CAMINAR"):
+			velocity.x = velocidad
+			$AnimatedSprite2D.flip_h = false
+			accion = "caminar"
+		elif Input.is_action_pressed("RETROCEDER"):
+			velocity.x =-velocidad
+			$AnimatedSprite2D.flip_h = true
+			accion = "retroceder"
+		else:
+			accion = ""
+			velocity.x = 0
+		if Input.is_action_pressed("CORRER adelante"):
+			velocity.x = velocidad*2
+			$AnimatedSprite2D.flip_h = false
+			accion="correr"
+		elif  Input.is_action_pressed("correr atras"):
+			velocity.x = velocidad *-2
+			$AnimatedSprite2D.flip_h = true
+			accion = "correr atras"
 
-	if ani == 1 or ani == -1:
-		$AnimatedSprite2D.play("avanzar")
-	elif ani == 2 or ani == -2:
-		$AnimatedSprite2D.play("correr")
-	if velocity.x < 0 if  velocity.x != 0  else $AnimatedSprite2D.flip_h:
-		$AnimatedSprite2D.flip_h = true
+		if is_on_floor():
+			if Input.is_action_just_pressed("SALTAR"):
+				velocity.y =-salto
+		if Input.is_action_just_pressed("atacar"):
+			atacar=true
 	else:
-		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("atacar")
+		await ($AnimatedSprite2D.animation_finished)
+		atacar=false
+		
+	animacionAciton(accion)
 	
-
 	move_and_slide()
+
+func animacionAciton(accion):
+	if accion == "correr" or accion == "correr atras":
+		$AnimatedSprite2D.play("correr")
+	elif accion == "caminar" or accion == "retroceder":
+		$AnimatedSprite2D.play("avanzar")
+
+	else:
+		$AnimatedSprite2D.play("detenido")
